df81500a5c0826005e8b8d08ad1b39da2dd97cf3
Oluwatobiloba <33703095+Tohbey@users.noreply.github.com>
2025-01-14 10:30:34 +0000

    introducing mysql. fixing bugs adding a new profile for mysql

--- src/main/java/com/example/spring_6_rest_mvc/model/Customer.java
+++ src/main/java/com/example/spring_6_rest_mvc/model/Customer.java
@@ -2,7 +2,9 @@ package com.example.spring_6_rest_mvc.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -18,7 +20,8 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @UuidGenerator
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     private String name;
