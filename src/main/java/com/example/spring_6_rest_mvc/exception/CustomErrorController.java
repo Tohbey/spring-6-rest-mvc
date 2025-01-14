bd18dcb3a527e84d399ac524fb6c2de034d0a390
Oluwatobiloba <33703095+Tohbey@users.noreply.github.com>
2025-01-14 09:46:19 +0000

    JPA validation test and method handling for data JPA Validation error.

--- src/main/java/com/example/spring_6_rest_mvc/exception/CustomErrorController.java
+++ src/main/java/com/example/spring_6_rest_mvc/exception/CustomErrorController.java
@@ -1,6 +1,8 @@
 package com.example.spring_6_rest_mvc.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +13,26 @@ import java.util.Map;
 
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    public ResponseEntity handleJPAValidations(TransactionSystemException exception){
+        ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+
+        if(exception.getCause().getCause() instanceof ConstraintViolationException){
+            ConstraintViolationException constraintViolationException = (ConstraintViolationException) exception.getCause().getCause();
+
+            List<Map<String, String>> errors = constraintViolationException.getConstraintViolations()
+                    .stream().map(constraintViolation -> {
+                        Map<String, String> errorMap = new HashMap<>();
+                        errorMap.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+                        return errorMap;
+                    }).toList();
+
+            return responseEntity.body(errors);
+        }
+
+        return responseEntity.build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity handleBindError(MethodArgumentNotValidException exception){
--- src/test/java/com/example/spring_6_rest_mvc/controller/BeerControllerTestIT.java
+++ src/test/java/com/example/spring_6_rest_mvc/controller/BeerControllerTestIT.java
@@ -5,22 +5,31 @@ import com.example.spring_6_rest_mvc.exception.NotFoundException;
 import com.example.spring_6_rest_mvc.mappers.BeerMapper;
 import com.example.spring_6_rest_mvc.model.Beer;
 import com.example.spring_6_rest_mvc.repositories.BeerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class BeerControllerTestIT {
@@ -32,6 +41,19 @@ class BeerControllerTestIT {
 
     @Autowired
     BeerMapper beerMapper;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
     void testBeerIdNotFound() {
@@ -127,5 +149,23 @@ class BeerControllerTestIT {
         assertThrows(NotFoundException.class, () -> {
             beerController.deleteById(UUID.randomUUID());
         });
+    }
+
+    @Test
+    void testPatchBeerBadName() throws Exception {
+        Beer beer = beerRepository.findAll().get(0);
+
+        Map<String, Object> beerMap = new HashMap<>();
+        beerMap.put("beerName", "New Name 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+
+        MvcResult mvcResult = mockMvc.perform(patch(BeerController.BEER_PATH+ "/" +beer.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerMap)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.length()", is(1)))
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
     }
 }
