7dba68b33ad3543afaeeee33015b13ecd81b86bc
Oluwatobiloba <33703095+Tohbey@users.noreply.github.com>
2025-01-14 09:30:47 +0000

    JPA validation.

--- src/test/java/com/example/spring_6_rest_mvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/example/spring_6_rest_mvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.example.spring_6_rest_mvc.repositories;
 
 import com.example.spring_6_rest_mvc.model.Beer;
+import com.example.spring_6_rest_mvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,11 +19,32 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    void testSaveBeerNameTooLong() {
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer 0123345678901233456789012334567890123345678901233456789012334567890123345678901233456789")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("234234234234")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+
+            beerRepository.flush();
+        });
+    }
+
+
+    @Test
     void saveBeer(){
         Beer savedBeer = beerRepository.save(Beer
                 .builder()
-                .beerName("New Beer")
+                .beerName("My Beer")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("234234234234")
+                .price(new BigDecimal("11.99"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
